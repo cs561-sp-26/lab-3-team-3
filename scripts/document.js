d07cb00621c85525b025eb53f01119ef6221296e
savvtann <savtan13@gmail.com>
2026-04-18 19:53:22 -0700

    Created document.js with header documentation

---
+++ scripts/document.js
@@ -0,0 +1,5 @@
+/*************************************************************************
+ * File: document.js
+ * These functions support interaction with the top-level document
+ * element.
+ ************************************************************************/
