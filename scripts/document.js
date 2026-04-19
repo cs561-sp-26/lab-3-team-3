19069978dc17431778fd466e7dbd0456274d8487
savvtann <savtan13@gmail.com>
2026-04-18 19:54:43 -0700

    added keydown event listener function skeleton

--- scripts/document.js
+++ scripts/document.js
@@ -3,3 +3,8 @@
  * These functions support interaction with the top-level document
  * element.
  ************************************************************************/
+
+
+document.addEventListener("keydown", function(e) { 
+
+});
