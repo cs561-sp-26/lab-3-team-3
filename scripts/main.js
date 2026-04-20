35ec0f7c2d29c3bcf82a1366d7e24e33b2d98246
Aj Protzel <ajprotzel@gmail.com>
2026-04-19 18:56:58 -0700

    Adrien (main.js) fixed spacing in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ let GlobalDialogClose = null;
 /************************************/
 /* MENU VARIABLES                   */
 /************************************/
-const GlobalMenuBtn = document.getElementById("menuBtn");
+const GlobalMenuBtn =  document.getElementById("menuBtn"); 
 const GlobalMenuItems = document.querySelectorAll("li[role='menuitem']");
 
 //Note: Per Josh Wulf's blog post, we implement all immutable global variables using
@@ -28,26 +28,26 @@ const GlobalFocusedMenuItem = (() => {
 /************************************/
 //Array of mode names
 const GlobalModeNumbersToModes = new Map([
-    [0, "ACTIVITY_FEED"],
-    [1, "ROUNDS"],
-    [2, "COURSES"],
-    [3, "BUDDIES"]
+  [0, "ACTIVITY_FEED"],
+  [1, "ROUNDS"],
+  [2, "COURSES"],
+  [3, "BUDDIES"]
 ]);
 const GlobalModeNumbersToRoutes = new Map([
-    [0, "/activityfeed"],
-    [1, "/rounds"],
-    [2, "/courses"],
-    [3, "/buddies"]
+  [0, "/activityfeed"],
+  [1, "/rounds"],
+  [2, "/courses"],
+  [3, "/buddies"]
 ]);
 const GlobalModeDialogNumbersToRoutes = new Map([
-    [0, "/activityfeed/newpost"],
-    [1, "/rounds/newround"],
-    [2, "/courses/addcourse"],
-    [3, "/buddies/findbuddy"]
+  [0, "/activityfeed/newpost"],
+  [1, "/rounds/newround"],
+  [2, "/courses/addcourse"],
+  [3, "/buddies/findbuddy"]
 ]);
 //The current mode (0, 1, 2, or 3)
 const GlobalCurrentMode = (() => {
-    let _currentMode = 0
+    let _currentMode= 0
     const Store = {
         get: () => _currentMode,
         set: val => (_currentMode = val)
@@ -56,7 +56,7 @@ const GlobalCurrentMode = (() => {
 })()
 
 const GlobalFocusedMode = (() => {
-    let _focusedMode = 0
+    let _focusedMode= 0
     const Store = {
         get: () => _focusedMode,
         set: val => (_focusedMode = val)
@@ -65,27 +65,27 @@ const GlobalFocusedMode = (() => {
 })()
 
 //Array of mode tab button elements:
-const GlobalModeTabButtons =
-    document.querySelectorAll("button[role='tab']");
+const GlobalModeTabButtons = 
+  document.querySelectorAll("button[role='tab']");
 //Array of mode tab panel elements:
-const GlobalModeTabPanels =
-    document.querySelectorAll("div[role='tabpanel']");
+const GlobalModeTabPanels = 
+  document.querySelectorAll("div[role='tabpanel']");
 
 /*****************************************************/
 /* FLOATING ACTION BUTTON AND MODAL DIALOG VARIABLES */
 /*****************************************************/
 //Array of mode action buttons
-const GlobalModeActionButtons =
-    document.querySelectorAll("button.float-btn");
+const GlobalModeActionButtons = 
+  document.querySelectorAll("button.float-btn");
 //array of mode action dialog boxes
 const GlobalModeActionDialogs =
-    document.querySelectorAll("div.action-dialog");
+  document.querySelectorAll("div.action-dialog");
 //array of "OK" buttons within the dialog boxes
 const GlobalDialogActionButtons =
-    document.querySelectorAll("button.action-button");
+  document.querySelectorAll("button.action-button");
 //array of "Cancel" buttons within the dialog boxes
 const GlobalDialogCancelButtons =
-    document.querySelectorAll("button.cancel-button");
+  document.querySelectorAll("button.cancel-button");
 
 /*****************************************************/
 /* OTHER UI COMPONENT VARIABLES */
