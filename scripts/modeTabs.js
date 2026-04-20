b5109b4f053fc691e53a04f38e45c34dda3b7806
Dravid <supermegaawesomedude3460@yahoo.com>
2026-04-19 17:48:31 -0700

    added file documentation

--- scripts/modeTabs.js
+++ scripts/modeTabs.js
@@ -1,4 +1,13 @@
 /*************************************************************************
+ * File: modeTabs.js
+ * Desc: Contains the JavaScript functions to handle interactions 
+ * with the mode tabs ("Feed", "Rounds", "Courses", "Buddies"). 
+ * We use the w3.org "Example of Tabs with Manual Activiation" as a 
+ * specification for implementing the accessible keyboard interface:
+ * https://www.w3.org/TR/wai-aria-practices/examples/tabs/tabs-2/tabs.html
+*************************************************************************/
+
+/*************************************************************************
  * @function switchMode 
  * @desc 
  * Switch from the current mode to a new mode. Unhighlight previous
